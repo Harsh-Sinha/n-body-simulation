f4fe180b521812ba1d8ce29d72429998ebecb651
Harsh-Sinha <harsh.sinha@hotmail.com>
2025-11-07 21:23:00 -0500

    update octree header; add image describing octant placement

--- octree/octree.h
+++ octree/octree.h
@@ -12,7 +12,9 @@ static constexpr size_t DEFAULT_MAX_POINTS_PER_NODE = 5;
 class Octree 
 { 
 public:
-    Octree(std::vector<std::shared_ptr<Point3d>>& leafs, bool supportMultithread = false, size_t maxPointsPerNode = DEFAULT_MAX_POINTS_PER_NODE);
+    Octree(std::vector<std::shared_ptr<Point3d>>& leafs, 
+           bool supportMultithread = false,
+           size_t maxPointsPerNode = DEFAULT_MAX_POINTS_PER_NODE);
     ~Octree() = default;
 
 private: 
@@ -20,12 +22,13 @@ private:
     
     struct BoundingBox
     {
-        std::array<double, 3> center{0.0, 0.0, 0.0}; 
+        std::array<double, 3> center{0.0, 0.0, 0.0};
         double halfOfSideLength = 0.0; 
         
-        bool pointInBox(const std::array<double, 3>& p) const
+        bool isPointInBox(const std::shared_ptr<Point3d>& point) const
         {
-            bool inBox = true; 
+            bool inBox = true;
+            auto& p = point->getPosition();
             
             inBox = inBox && std::abs(p[0] - center[0]) <= halfOfSideLength;
             inBox = inBox && std::abs(p[1] - center[1]) <= halfOfSideLength;
@@ -38,46 +41,61 @@ private:
     struct Node
     { 
         BoundingBox boundingBox; 
-        std::array<std::unique_ptr<Node>, 8> children;
-        std::shared_ptr<Point3d> leaf;
+        std::array<std::shared_ptr<Node>, 8> octants;
+        std::vector<std::shared_ptr<Point3d>> points;
+        std::shared_ptr<Node> parentNode;
         
         bool isLeafNode() const
         {
             bool isLeaf = true;
             
-            for (const auto& child : children) 
+            for (const auto& octant : octants) 
             {
-                isLeaf = isLeaf && child == nullptr;
+                isLeaf = isLeaf && octant == nullptr;
             } 
             
             return isLeaf;
         } 
         
-        bool hasPoint() const
+        bool containsPoints() const
         {
-            return leaf != nullptr;
+            return points.size() > 0;
         }
     };
 
-    BoundingBox computeBoundingBox(std::vector<std::shared_ptr<Point3d>>& leafs);
+    BoundingBox computeBoundingBox(std::vector<std::shared_ptr<Point3d>>& points);
     
-    void insert(std::unique_ptr<Node>& node, std::shared_ptr<Point3d>& leaf);
+    void insert(std::unique_ptr<Node>& node, std::shared_ptr<Point3d>& point);
     
     void splitBoundingBox(std::unique_ptr<Node>& node);
     
     BoundingBox createChildBox(int index, const BoundingBox& parent); 
     
-    inline int toChildIndex(const std::array<double, 3>& p, const BoundingBox& box) 
+    inline size_t toOctantId(const std::shared_ptr<Point3d>& point, const BoundingBox& box) 
     {
-        int ix = (p[0] >= box.center[0]) ? 1 : 0;
-        int iy = (p[1] >= box.center[1]) ? 1 : 0;
-        int iz = (p[2] >= box.center[2]) ? 1 : 0;
+        auto& p = point->getPosition();
+
+        // decide if point is in upper or lower quadrant
+        size_t id = (p[2] >= box.center[2]) ? 0 : 4;
+
+        // use quadrant rules to place point
+        // (+,+) - quandrant 0
+        // (-,+) - quandrant 1
+        // (-,-) - quandrant 2
+        // (+,-) - quandrant 3
+        if (p[0] >= box.center[0]) // (+,?)
+        {
+            id += (p[1] >= box.center[1]) ? 0 : 3;
+        }
+        else // (-,?)
+        {
+            id += (p[1] >= box.center[1]) ? 1 : 2;
+        }
         
-        return (ix) | (iy << 1) | (iz << 2);
-    
-    } 
-    
-    std::unique_ptr<Node> mRoot = std::make_unique<Node>(); 
+        return id;
+    }
+
+    std::shared_ptr<Node> mRoot = std::make_shared<Node>();
     bool mSupportMultithread;
     size_t mMaxPointsPerNode;
 };
