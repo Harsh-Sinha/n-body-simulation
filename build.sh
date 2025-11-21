034e59097c5d9d82ba3d3768a5b06756b833dae7
Harsh-Sinha <harsh.sinha@hotmail.com>
2025-11-21 11:44:07 -0500

    add scripts to help build

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -d build ]; then
+    echo "deleting existing build directory"
+    rm -rf build
+fi
+
+if [ -d install ]; then
+    echo "deleting existing install directory"
+    rm -rf install
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=install -DALEMBIC_SHARED_LIBS=OFF -DENABLE_TESTING=OFF -DUSE_TESTS=OFF -B build
+
+cmake --build build
+
+cmake --built build --target install
